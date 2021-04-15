8f74148ed4e8a2c371cb7498d306c9003a499019
Amine <Amine@MacBook-Pro-de-Amin.local>
2021-04-15 17:27:16 +0100

    add existing project to github

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9829a8901f09d8661610c8055b05dd37'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e0a1f78-f84b-4e09-824c-6d88edafc83f'
+};
